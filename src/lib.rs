e5624f51e06977859acd81c62f0ec66368e8b927
someone1351 <someone1351@users.noreply.github.com>
2025-07-27 21:27:12 +1000

    some commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,10 @@
 pub mod core;
 pub mod mesh;
+/*
+
+TODO
+* add key for transparent pipeline
+* add texture support, dummy image
+* handle/test render layers
+* add opaque, alpha key pipelines to mesh
+*/
