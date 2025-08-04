ebf78e7882474152c06d9cea962bd0a85cf8aba3
someone1351 <someone1351@users.noreply.github.com>
2025-08-05 08:06:05 +1000

    some commit

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,6 @@ pub mod mesh;
 /*
 
 TODO
-* add key for transparent pipeline, msaa for pipeline
-* add main entity to transparent phase
-* make sure only extract entities to draw for my camera
-* handle/test render layers
-
-* add texture support, dummy image
-* test render to
+* add key for transparent pipeline,
 * add opaque, alpha key pipelines to mesh
 */
